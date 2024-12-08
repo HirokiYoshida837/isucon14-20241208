53a0b56be7c834141bf94139e47b9ad750b96b07
h.yoshida <hiroki.yoshida01@outlook.jp>
2024-12-08 11:22:45 +0900

    chi tracer fix

--- go/main.go
+++ go/main.go
@@ -80,7 +80,7 @@ func setup() http.Handler {
 	mux := chi.NewRouter()
 
 	//// Use the tracer middleware with the default service name "chi.router".
-	mux.Use(chitrace.Middleware())
+	mux.Use(chitrace.Middleware(chitrace.WithServiceName("chi-server")))
 
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
